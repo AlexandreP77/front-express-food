8e85da27bdf11fec510d4d28108671a073267ca3
AlexandreP77 <alexandredasilva.piedade@gmail.com>
2024-06-21 11:00:18 +0200

    host

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port: port,
+    host: '0.0.0.0',
     proxy: {
       '/commandes': {
         target: 'https://python-api-prod.onrender.com',
