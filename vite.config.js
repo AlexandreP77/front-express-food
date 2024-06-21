485749c7f1a2655e22f11f898734d2422682c55f
AlexandreP77 <alexandredasilva.piedade@gmail.com>
2024-06-21 10:51:50 +0200

    port changer

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 10000,
     proxy: {
       '/commandes': {
         target: 'https://python-api-prod.onrender.com',
